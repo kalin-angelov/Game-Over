24f3e929ced86d84461f2b856446542aa28c89a8
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-03-22 18:46:29 +0200

    Create authService.js

---
+++ Game Over/client/src/service/authService.js
@@ -0,0 +1,43 @@
+const baseUrl = 'http://localhost:3030/users';
+
+export const register = async (body) => {
+    const response = await fetch(`${baseUrl}/register`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    if (response.status === 200) {
+        const result = await response.json();
+
+        return result;
+    }
+
+    if (response.status === 204) {
+        return {};
+    }
+
+    throw await response.json();
+};
+
+export const login = async (body) => {
+    const response = await fetch(`${baseUrl}/login`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    });
+
+    if (response.status === 200) {
+        const result = await response.json();
+
+        return result;
+    } else {
+        const error = await response.json()
+
+        throw error;
+    }
+};
