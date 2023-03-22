d5ce272d792a0b039d69bbb7e85b1e744a00c1e9
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-03-22 18:39:18 +0200

    Create Footer.js

---
+++ Game Over/client/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="footer">
+                <div className="container">
+                    <p>© 2019 All Rights Reserved. <a href="https://html.design/">Free html Templates</a></p>
+                </div>
+            </div>
+        </footer>
+    );
+};
