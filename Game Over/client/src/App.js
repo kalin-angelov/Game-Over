11e23c26482552bf48805c888034a5aac96fb232
kalin-angelov <122154846+kalin-angelov@users.noreply.github.com>
2023-03-23 19:29:55 +0200

    Update App.js

--- Game Over/client/src/App.js
+++ Game Over/client/src/App.js
@@ -2,7 +2,6 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import { getAll, addOne, updateOne, deleteOne } from './service/gameService';
-import { register, login } from './service/authService';
 import { AuthContext } from './contexts/AuthContext';
 
 import { Header } from "./components/Header/Header";
@@ -16,7 +15,7 @@ import { Edit } from "./components/Edit/Edit";
 import { Home } from "./components/Home/Home";
 import { Profile } from "./components/Profile/Profile";
 import { Details } from "./components/Details/Details";
-import { PageNotFound } from "./components/404/PageNotFound"
+import { PageNotFound } from "./components/404/PageNotFound";
 
 function App() {
   const navigate = useNavigate();
@@ -26,6 +25,7 @@ function App() {
 
   useEffect(() => {
     getAll()
+      .then(gamesList => Object.values(gamesList))
       .then(gamesList => setGameList(gamesList))
       .catch(err => console.log(`Error ${err}`))
   }, []);
@@ -39,7 +39,7 @@ function App() {
       const result = await getAll();
 
       setGameList(result);
-      navigate('/catalog');
+      navigate('/profile');
     } catch (err) {
       console.log(`Error: ${err}`);
       navigate('/404');
@@ -76,94 +76,49 @@ function App() {
 
       setGameList(result);
       onClickCloseDelete();
-      navigate('/catalog');
+      navigate('/profile');
 
     } catch (err) {
       console.log(`Error: ${err}`);
     }
-  };
-
-  const onRegister = async (e, body) => {
-    e.preventDefault();
-
-    const {rePassword, ...bodyData} = body;
-
-    if (rePassword !== bodyData.password) {
-      return;
-    }
-
-    try {
-      const response = await register(bodyData);
-
-      setAuth(response);
-
-      navigate('/catalog');
-    } catch(err) {
-      console.log(`Error: ${err}`);
-    }
-   
-  };
-
-  const onLogin = async (e, body) => {
-    e.preventDefault();
-
-    try {
-      const result = await login(body);
-
-      setAuth(result);
-
-      navigate('/catalog')
-
-      console.log(auth);
-    } catch (err) {
-      console.log(`Error: ${err}`);
-    }
-  };
-
-  const onLogout = () => {
-    setAuth({});
   };
 
   const valueContext = {
-    onRegister,
-    onLogin,
-    onLogout,
+    setAuth,
     userId: auth._id,
     username: auth.username,
     email: auth.email,
     toke: auth.accessToken,
-    isAuthenticated: !!auth.accessToken
+    isAuthenticated: !!auth.accessToken,
+    gamesList,
+    onEditSubmit,
+    onAddNewGameSubmit,
+    onDeleteGame,
+    onClickShowDelete,
+    onClickCloseDelete,
+    showDelete,
   };
 
-  
+
   return (
     <AuthContext.Provider value={valueContext}>
-      <>
-        <div className='main'>
-          <Header />
-          <Routes>
-            <Route path='*' element={<PageNotFound />} />
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/logout' element={<Logout />} />
-            <Route path='/profile' element={<Profile gamesList={gamesList} />} />
-            <Route path='/catalog' element={<Catalog gamesList={gamesList} />} />
-            <Route path='/create' element={<Create onAddNewGameSubmit={onAddNewGameSubmit} />} />
-            <Route path='/edit/:gameId' element={<Edit onEditSubmit={onEditSubmit} />} />
-            <Route path='/details/:gameId' element={
-              <Details
-                onDeleteGame={onDeleteGame}
-                onClickShowDelete={onClickShowDelete}
-                onClickCloseDelete={onClickCloseDelete}
-                showDelete={showDelete}
-              />
-            } />
-          </Routes>
+      <div className='main'>
+        <Header />
+        <Routes>
+          <Route path='*' element={<PageNotFound />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/logout' element={<Logout />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/edit/:gameId' element={<Edit />} />
+          <Route path='/details/:gameId' element={<Details />} />
+        </Routes>
 
-          <Footer />
-        </div>
-      </>
+        <Footer />
+      </div>
     </AuthContext.Provider>
   );
 }
