26a955e0b72132a79bf5e77b3493607878c3a2d5
kalin-angelov <kalinangelov@abv.bg>
2023-03-23 19:54:30 +0200

    Update Home.js

--- Game Over/client/src/components/Home/Home.js
+++ Game Over/client/src/components/Home/Home.js
@@ -6,9 +6,7 @@ export const Home = () => {
                     <div className="row">
                         <div className="col-xl-4 col-lg-4 col-md-4 col-sm-12 ">
                             <div className="text-bg">
-                                <h1>amazing 3d game</h1>
-                                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</span>
-                                <a href="#">download</a>
+                                <span>Here You Can Find Advice On How To Beat The Game That Is Giving You A Hard Time </span>
                             </div>
                         </div>
                         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 ">
