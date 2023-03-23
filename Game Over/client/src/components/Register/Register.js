7711b16e053efa2d31ab18680ca4bed77bce296f
kalin-angelov <kalinangelov@abv.bg>
2023-03-23 19:57:58 +0200

    Update Register.js

--- Game Over/client/src/components/Register/Register.js
+++ Game Over/client/src/components/Register/Register.js
@@ -1,18 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 
 import { AuthContext } from "../../contexts/AuthContext";
 import { useForm } from "../../hooks/useForm";
+import { register } from '../../service/authService';
 
 
 export const Register = () => {
-    const { onRegister } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { setAuth } = useContext(AuthContext);
     const { formValue, onFormValueChange } = useForm({
         username: '',
         email: '',
         password: '',
         rePassword: '',
     });
+
+    const onRegister = async (e) => {
+        e.preventDefault();
+
+        const { rePassword, ...bodyData } = formValue;
+
+        if (rePassword !== bodyData.password) {
+            return;
+        }
+
+        try {
+            const response = await register(bodyData);
+
+            setAuth(response);
+
+            navigate('/catalog');
+        } catch (err) {
+            console.log(`Error: ${err}`);
+        }
+
+    };
 
     return (
         <div id="contact" className="register">
@@ -21,50 +44,50 @@ export const Register = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <form className="register-form" onSubmit={(e) => onRegister(e, formValue)} >
+                        <form className="register-form" onSubmit={onRegister} >
                             <div className="row">
                                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
                                     <label htmlFor="username">Username: </label>
-                                    <input 
+                                    <input
                                         className="username"
-                                        placeholder="Username" 
-                                        type="text" 
-                                        name="username" 
-                                        value={formValue.username} 
-                                        onChange={onFormValueChange} 
+                                        placeholder="Username"
+                                        type="text"
+                                        name="username"
+                                        value={formValue.username}
+                                        onChange={onFormValueChange}
                                     />
                                 </div>
                                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
                                     <label htmlFor="email">Email: </label>
-                                    <input 
-                                        className="email" 
-                                        placeholder="Email" 
-                                        type="text" 
-                                        name="email" 
-                                        value={formValue.email} 
-                                        onChange={onFormValueChange} 
+                                    <input
+                                        className="email"
+                                        placeholder="Email"
+                                        type="text"
+                                        name="email"
+                                        value={formValue.email}
+                                        onChange={onFormValueChange}
                                     />
                                 </div>
                                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
                                     <label htmlFor="password">Password: </label>
-                                    <input 
-                                        className="password" 
-                                        placeholder="Password" 
-                                        type="password" 
-                                        name="password" 
-                                        value={formValue.password} 
+                                    <input
+                                        className="password"
+                                        placeholder="Password"
+                                        type="password"
+                                        name="password"
+                                        value={formValue.password}
                                         onChange={onFormValueChange}
                                     />
                                 </div>
                                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
                                     <label htmlFor="rePassword">Confirm Password: </label>
-                                    <input 
-                                        className="rePassword" 
-                                        placeholder="Confirm Password" 
-                                        type="password" 
-                                        name="rePassword" 
-                                        value={formValue.rePassword} 
-                                        onChange={onFormValueChange} 
+                                    <input
+                                        className="rePassword"
+                                        placeholder="Confirm Password"
+                                        type="password"
+                                        name="rePassword"
+                                        value={formValue.rePassword}
+                                        onChange={onFormValueChange}
                                     />
                                 </div>
                                 <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
