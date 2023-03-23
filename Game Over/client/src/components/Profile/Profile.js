2a4a69975d27421cc1a48bd1d2ad6b663ee8907a
kalin-angelov <kalinangelov@abv.bg>
2023-03-23 19:57:18 +0200

    Update Profile.js

--- Game Over/client/src/components/Profile/Profile.js
+++ Game Over/client/src/components/Profile/Profile.js
@@ -1,32 +1,42 @@
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { MyGames } from './MyGames';
 import { AuthContext } from '../../contexts/AuthContext';
+import { getAll } from '../../service/gameService';
+import { userGameCheck } from '../../utils/userGameCheck';
 
-export const Profile = ({
-    gamesList
-}) => {
-    const { username } = useContext(AuthContext);
+export const Profile = () => {
+    const { username, userId } = useContext(AuthContext);
+    const [ userGames, setUserGames ] = useState([]);
+
+    useEffect(() => {
+        getAll()
+            .then(data => userGameCheck(data, userId))
+            .then(data => setUserGames(data))
+            .catch(error => console.log(`Error: ${error}`))
+    },[userId]);
 
     return (
         <div className="profile">
             <div className="titlePage">
                 <h2>Welcome: {username}</h2>
             </div>
-            <div className="brand-logo"></div>
+
+            <Link to='/create'><p className="brand-logo"></p></Link>
+           
             <div className="list">
                 <h3>Game's List</h3>
             </div>
             
-            {(gamesList.length > 0 ) ?
+            {(userGames.length > 0 ) ?
                 <div className='games-list'>
-                    {gamesList.map(game => <MyGames key={game._id} game={game}/>)}  
+                    {userGames.map(game => <MyGames key={game._id} game={game}/>)}  
                 </div>
                 :
                 <div className='no_games'>
                     <h3>Your List Is Empty</h3>
-                    <p> You Can Change That From Here! <Link to='/create'>Add Game</Link> </p>
+                    <p> You Can Change That By Clicking On The Big Red Controller!</p>
                 </div> 
             }
         </div>
