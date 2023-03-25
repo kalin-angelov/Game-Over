2397d1772bfc38f7c6439554e29d8e740db8cf34
kalin-angelov <kalinangelov@abv.bg>
2023-03-25 18:18:44 +0200

    Update Footer.js With Styles

--- Game Over/client/src/components/Footer/Footer.js
+++ Game Over/client/src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
+import styles from './Footer.module.css';
+
 export const Footer = () => {
     return (
         <footer>
-            <div className="footer">
-                <div className="container">
-                    <p>© 2019 All Rights Reserved. <a href="https://html.design/">Free html Templates</a></p>
-                </div>
+            <div className={styles.footer}>
+                <p>© 2019 All Rights Reserved. <a href="https://html.design/">Free html Templates</a></p>
             </div>
         </footer>
     );
