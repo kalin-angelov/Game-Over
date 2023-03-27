74f42652166a16c484b7a5c7bfef0ee1807a700b
kalin-angelov <kalinangelov@abv.bg>
2023-03-27 21:14:50 +0300

    Update PageNotFound.js

--- Game Over/client/src/components/404/PageNotFound.js
+++ Game Over/client/src/components/404/PageNotFound.js
@@ -1,12 +1,13 @@
+import styles from './PageNotFound.module.css';
+
 import { Link } from 'react-router-dom';
 
 export const PageNotFound = () => {
     return (
-        <div className='ups'>
-            <div className="titlePage">
-                <h2>404</h2>
-            </div>
-            <div className='page-not-found'>
+        <div className={styles.ups}>
+            
+            <h2>404</h2>
+            <div>
                 <h3>Page Not Found</h3>
                 <p>
                     The Page You Are Looking For Doesn't Exist Or Another Error Occurred. Go.......
