08541da99d88aa09242abf23b053309891fc53a5
kalin-angelov <kalinangelov@abv.bg>
2023-03-27 21:26:10 +0300

    Update Register.js

--- Game Over/client/src/components/Register/Register.js
+++ Game Over/client/src/components/Register/Register.js
@@ -1,3 +1,5 @@
+import styles from './Register.module.css';
+
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 
@@ -41,70 +43,59 @@ export const Register = () => {
     };
 
     return (
-        <div id="contact" className="register">
-            <div className="brand-logo"></div>
-            <h1 className="titlePage">Register Form</h1>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <form className="register-form" onSubmit={onRegister} >
-                            <div className="row">
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="username">Username: </label>
-                                    <input
-                                        className="username"
-                                        placeholder="Username"
-                                        type="text"
-                                        name="username"
-                                        value={formValue.username}
-                                        onChange={onFormValueChange}
-                                    />
-                                </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="email">Email: </label>
-                                    <input
-                                        className="email"
-                                        placeholder="Email"
-                                        type="text"
-                                        name="email"
-                                        value={formValue.email}
-                                        onChange={onFormValueChange}
-                                    />
-                                </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="password">Password: </label>
-                                    <input
-                                        className="password"
-                                        placeholder="Password"
-                                        type="password"
-                                        name="password"
-                                        value={formValue.password}
-                                        onChange={onFormValueChange}
-                                    />
-                                </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="rePassword">Confirm Password: </label>
-                                    <input
-                                        className="rePassword"
-                                        placeholder="Confirm Password"
-                                        type="password"
-                                        name="rePassword"
-                                        value={formValue.rePassword}
-                                        onChange={onFormValueChange}
-                                    />
-                                </div>
-                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <button className="send">Register</button>
-                                </div>
-                                <div className="register-info">
-                                    <p> Already Have Account? Click Here! <Link to='/login'>Login</Link> </p>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+        <div className={styles.register}>
+            <div className={styles.brandLogo}></div>
 
-            </div>
+            <form className={styles.registerForm} onSubmit={onRegister} >
+                <h1 >Register Form</h1>
+
+                <label htmlFor="username">Username: </label>
+                <input
+                    className={styles.username}
+                    placeholder="Username"
+                    type="text"
+                    name="username"
+                    value={formValue.username}
+                    onChange={onFormValueChange}
+                />
+
+                <label htmlFor="email">Email: </label>
+                <input
+                    className={styles.email}
+                    placeholder="Email"
+                    type="text"
+                    name="email"
+                    value={formValue.email}
+                    onChange={onFormValueChange}
+                />
+
+                <label htmlFor="password">Password: </label>
+                <input
+                    className={styles.password}
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    value={formValue.password}
+                    onChange={onFormValueChange}
+                />
+
+                <label htmlFor="rePassword">Confirm Password: </label>
+                <input
+                    className={styles.rePassword}
+                    placeholder="Confirm Password"
+                    type="password"
+                    name="rePassword"
+                    value={formValue.rePassword}
+                    onChange={onFormValueChange}
+                />
+
+                <div>
+                    <button className={styles.send}>Register</button>
+                </div>
+                <div className={styles.registerInfo}>
+                    <p> Already Have Account? Click Here! <Link to='/login'>Login</Link> </p>
+                </div>
+            </form>
         </div>
     );
 };
