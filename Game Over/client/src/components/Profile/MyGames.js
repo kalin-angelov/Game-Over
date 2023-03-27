9c051b6ab731d1392eec7ddbbd6e6b230ab78bfc
kalin-angelov <kalinangelov@abv.bg>
2023-03-27 21:25:06 +0300

    Update MyGames.js

--- Game Over/client/src/components/Profile/MyGames.js
+++ Game Over/client/src/components/Profile/MyGames.js
@@ -1,14 +1,15 @@
+import styles from './Profile.module.css';
+
 import { Link } from 'react-router-dom';
 
 export const MyGames = ({ game }) => {
     return (
-        <div className="two-items">
+        <div className={styles.myGames}>
+            <h1>{game.title}</h1>
+
             <Link to={`/details/${game._id}`} >
                 <img src={game.imageUrl} alt="{game.title}" />
             </Link>
-            <div className='title'>
-                <h1>{game.title}</h1>
-            </div>
         </div>
     );
 };
