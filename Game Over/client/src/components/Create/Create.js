2069339aac87c45d9f78b04eb496f5cce53a05f0
kalin-angelov <kalinangelov@abv.bg>
2023-03-27 21:20:15 +0300

    Update Create.js

--- Game Over/client/src/components/Create/Create.js
+++ Game Over/client/src/components/Create/Create.js
@@ -1,103 +1,93 @@
-import { useForm } from '../../hooks/useForm';
+import styles from './Create.module.css';
 
-export const Create = ({
-    onAddNewGameSubmit
-}) => {
+import { useContext } from 'react';
+
+import { useForm } from '../../hooks/useForm';
+import { AuthContext } from '../../contexts/AuthContext';
+
+export const Create = () => {
+    const { onAddNewGameSubmit } = useContext(AuthContext);
     const { formValue, onFormValueChange } = useForm({
         title: '',
         genre: '',
         platform: '',
         players: '',
         imageUrl: '',
-        summary: ''
+        summary: '',
+        comments: []
     });
 
     return (
-        <div id="contact" className="create">
-            <div className="container">
-                <div className="brand-logo"></div>
-                <h1 className="titlePage">Add New Game</h1>
-                <div className="row">
+        <div className={styles.create}>
+            <div className={styles.brandLogo}></div>
+            <form className={styles.createForm} onSubmit={(e) => onAddNewGameSubmit(e, formValue)}>
+                <h1>Add New Game</h1>
 
-                    <div className="col-md-12">
+                <label htmlFor="title">Title: </label>
+                <input
+                    className={styles.title}
+                    placeholder="Title"
+                    type="text"
+                    name="title"
+                    value={formValue.title}
+                    onChange={onFormValueChange}
+                />
 
-                        <form className="create-form" onSubmit={(e) => onAddNewGameSubmit(e, formValue)}>
-                            <div className="row">
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="title">Title: </label>
-                                    <input 
-                                        className="title" 
-                                        placeholder="Title" 
-                                        type="text" 
-                                        name="title" 
-                                        value={formValue.title} 
-                                        onChange={onFormValueChange} 
-                                    />
-                                </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="genre">Genre: </label>
-                                    <input 
-                                        className="genre" 
-                                        placeholder="Genre" 
-                                        type="text" 
-                                        name="genre" 
-                                        value={formValue.genre} 
-                                        onChange={onFormValueChange} 
-                                    />
-                                </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="platform">Platform: </label>
-                                    <input 
-                                        className="platform" 
-                                        placeholder="Platform" 
-                                        type="text" 
-                                        name="platform" 
-                                        value={formValue.platform} 
-                                        onChange={onFormValueChange} 
-                                    />
-                                </div>
-                                <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                    <label htmlFor="players">Number Of Players: </label>
-                                    <input 
-                                        className="players" 
-                                        placeholder="Players" 
-                                        type="text" 
-                                        name="players" 
-                                        value={formValue.players} 
-                                        onChange={onFormValueChange} 
-                                    />
-                                </div>
-                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <label htmlFor="imageUrl">ImageUrl: </label>
-                                    <input 
-                                        className="imageUrl" 
-                                        placeholder="ImageUrl" 
-                                        type="text" 
-                                        name="imageUrl" 
-                                        value={formValue.imageUrl} 
-                                        onChange={onFormValueChange} 
-                                    />
-                                </div>
-                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <label htmlFor="summary">Summary: </label>
-                                    <textarea 
-                                        className="summary" 
-                                        placeholder="Summary" 
-                                        type="text" 
-                                        name="summary" 
-                                        value={formValue.summary} 
-                                        onChange={onFormValueChange} 
-                                    ></textarea>
-                                </div>
-                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                                    <button className="send">Save</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                <label htmlFor="genre">Genre: </label>
+                <input
+                    className={styles.genre}
+                    placeholder="Genre"
+                    type="text"
+                    name="genre"
+                    value={formValue.genre}
+                    onChange={onFormValueChange}
+                />
 
-            </div>
+                <label htmlFor="platform">Platform: </label>
+                <input
+                    className={styles.platform}
+                    placeholder="Platform"
+                    type="text"
+                    name="platform"
+                    value={formValue.platform}
+                    onChange={onFormValueChange}
+                />
+
+                <label htmlFor="players">Number Of Players: </label>
+                <input
+                    className={styles.players}
+                    placeholder="Players"
+                    type="text"
+                    name="players"
+                    value={formValue.players}
+                    onChange={onFormValueChange}
+                />
+
+                <label htmlFor="imageUrl">ImageUrl: </label>
+                <input
+                    className={styles.imageUrl}
+                    placeholder="ImageUrl"
+                    type="text"
+                    name="imageUrl"
+                    value={formValue.imageUrl}
+                    onChange={onFormValueChange}
+                />
+
+                <label htmlFor="summary">Summary: </label>
+                <textarea
+                    className={styles.summary}
+                    placeholder="Summary"
+                    type="text"
+                    name="summary"
+                    value={formValue.summary}
+                    onChange={onFormValueChange}
+                ></textarea>
+
+                <article>
+                    <button className={styles.send}>Save</button>
+                </article>
+
+            </form>
         </div>
     );
 };
