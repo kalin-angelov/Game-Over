112fbc73775c2ae2c544a1ae9305cd080a0bc509
kalin-angelov <kalinangelov@abv.bg>
2023-03-29 15:42:48 +0300

    Update Game.js

--- Game Over/client/src/components/Catalog/Game.js
+++ Game Over/client/src/components/Catalog/Game.js
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 export const Game = (game) => {
     return (
         <div className={styles.game}>
-            <img src={game.imageUrl} alt="{game.title}" />
-
+            <Link to={`/details/${game._id}`} state={game}>
+                <img src={game.imageUrl} alt="{game.title}" />
+            </Link>
+            
             <div className={styles.gameInfo}>
-                <h3>{game.title}</h3>
-                <p>Players: {game.players}</p>
+                <h3>Title: {game.title}</h3>
+                <p>Help: {game.help}</p>
                 <p>Platform: {game.platform}</p>
-                <p>Genre: {game.genre}</p>
-                <Link to={`/details/${game._id}`} state={game}>Details</Link>
             </div>
         </div>
     );
