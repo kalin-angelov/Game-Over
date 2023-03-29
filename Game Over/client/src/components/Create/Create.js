073b9c869ecc80072d9562b58668637c790c2bbe
kalin-angelov <kalinangelov@abv.bg>
2023-03-29 15:43:16 +0300

    Update Create.js

--- Game Over/client/src/components/Create/Create.js
+++ Game Over/client/src/components/Create/Create.js
@@ -6,11 +6,12 @@ import { useForm } from '../../hooks/useForm';
 import { AuthContext } from '../../contexts/AuthContext';
 
 export const Create = () => {
-    const { onAddNewGameSubmit } = useContext(AuthContext);
+    const { onAddNewGameSubmit, errorMessage } = useContext(AuthContext);
     const { formValue, onFormValueChange } = useForm({
         title: '',
-        genre: '',
+        help: '',
         platform: '',
+        genre: '',
         players: '',
         imageUrl: '',
         summary: '',
@@ -19,6 +20,11 @@ export const Create = () => {
 
     return (
         <div className={styles.create}>
+            {errorMessage &&
+                <div className={styles.error}>
+                    <p>{errorMessage}</p>
+                </div>
+            }
             <div className={styles.brandLogo}></div>
             <form className={styles.createForm} onSubmit={(e) => onAddNewGameSubmit(e, formValue)}>
                 <h1>Add New Game</h1>
@@ -33,6 +39,32 @@ export const Create = () => {
                     onChange={onFormValueChange}
                 />
 
+                <label htmlFor="help">Help: </label>
+                <select 
+                    className={styles.help} 
+                    name='help' 
+                    value={formValue.help}
+                    onChange={onFormValueChange}>
+                        <option>Other</option>
+                        <option>Location</option>
+                        <option>Item</option>
+                        <option>Boss</option>
+                        <option>Skills</option>
+                </select>
+
+                <label htmlFor="platform">Platform: </label>
+                <select 
+                    className={styles.platform} 
+                    name='platform' 
+                    value={formValue.platform}
+                    onChange={onFormValueChange}>
+                        <option>PC</option>
+                        <option>PlayStation</option>
+                        <option>Xbox</option>
+                        <option>Nintendo</option>
+                        <option>Other</option>
+                </select>
+
                 <label htmlFor="genre">Genre: </label>
                 <input
                     className={styles.genre}
@@ -40,16 +72,6 @@ export const Create = () => {
                     type="text"
                     name="genre"
                     value={formValue.genre}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="platform">Platform: </label>
-                <input
-                    className={styles.platform}
-                    placeholder="Platform"
-                    type="text"
-                    name="platform"
-                    value={formValue.platform}
                     onChange={onFormValueChange}
                 />
 
