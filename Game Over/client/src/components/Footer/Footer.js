98865802866db7f5d760900bde9b6298c48a3204
kalin-angelov <kalinangelov@abv.bg>
2023-04-02 17:26:54 +0300

    Update Footer.js

--- Game Over/client/src/components/Footer/Footer.js
+++ Game Over/client/src/components/Footer/Footer.js
@@ -1,13 +1,11 @@
-import styles from './Home.module.css';
+import styles from './Footer.module.css';
 
-export const Home = () => {
+export const Footer = () => {
     return (
-        <section >   
-            <div className={styles.home}> 
-                <p>Here You Can Find Advice On How To Beat The Game That Is Giving You A Hard Time </p>
-                
-                <img src="images/img.png" alt="homeImage" />
+        <footer>
+            <div className={styles.footer}>
+                <p>NooB!, Welcome To GAME OVER ;)</p>
             </div>
-        </section>
+        </footer>
     );
 };
