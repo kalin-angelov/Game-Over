e35e3b74bd192f0157d67414ad3c14e2ff606769
kalin-angelov <kalinangelov@abv.bg>
2023-04-02 17:33:31 +0300

    Update gameCommentService.js

--- Game Over/client/src/service/gameCommentService.js
+++ Game Over/client/src/service/gameCommentService.js
@@ -2,44 +2,64 @@ import { bodyCheck } from '../utils/bodyCheck';
 
 const baseUrl = 'http://localhost:3030/jsonstore';
 
-export const addOneComment = async (id, body) => {
-    try {
-        bodyCheck(body);
+const request = async (url, method, body) => {
 
-        const response = await fetch(`${baseUrl}/${id}`, {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(body)
-        });
-        const result = await response.json();
-
-        return result;
-    } catch (err) {
-        throw Error(`Error: ${err}`);
+    const options = {
+        method: method,
+        headers: {}
     }
 
-};
+    if (body) {
+        options.headers = {
+            'content-type': 'application/json',
+        }
+        options.body = JSON.stringify(body)
+    }
 
-export const getAllComments = async (id) => {
     try {
-        
-        const response = await fetch(`${baseUrl}/${id}`);
+        if (body) {
+            bodyCheck(body);
+        }
+
+        const response = await fetch(url, options);
 
         if (response.status === 200) {
             const result = await response.json();
-            const comments = Object.values(result);
 
-            return comments;
+            return result;
         } else if (response.status === 204) {
 
             return [];
         }
 
         return [];
+
     } catch (err) {
-        throw Error(`Error: ${err}`);
+        throw Error(err.message);
     }
 
+}
+
+export const addOneComment = async (id, body) => {
+    return request(`${baseUrl}/${id}`,"POST", body);
+};
+
+export const getAllComments = async (id) => {
+    return request(`${baseUrl}/${id}`,"GET");
+};
+
+export const getOneComment = async (gameId,id) => {
+    return request(`${baseUrl}/${gameId}/${id}`,"GET");
+};
+
+export const deleteComment = async (gameId,id) => {
+    return request(`${baseUrl}/${gameId}/${id}`,"DELETE");
+};
+
+export const updateComment = async (gameId, id, body) => {
+    return request(`${baseUrl}/${gameId}/${id}`,"PUT", body);
+};
+
+export const likeComment = async (gameId, id, body) => {
+    return request(`${baseUrl}/${gameId}/${id}`,"PUT", body);
 };
