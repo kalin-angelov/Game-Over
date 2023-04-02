6fbb7dc89c0ef5a5c3c65a5604c3734cd116df09
kalin-angelov <kalinangelov@abv.bg>
2023-04-02 17:29:48 +0300

    Update MyGames.js

--- Game Over/client/src/components/Profile/MyGames.js
+++ Game Over/client/src/components/Profile/MyGames.js
@@ -1,15 +1,35 @@
 import styles from './Profile.module.css';
 
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
-export const MyGames = ({ game }) => {
+import { DeleteButton } from './DeleteButton';
+
+export const MyGames = ({ 
+    game,
+    showDelete,
+    onDeleteGame,
+    onClickShowDelete,
+    onClickCloseDelete
+ }) => {
     return (
         <div className={styles.myGames}>
             <h1>{game.title}</h1>
-
+            
             <Link to={`/details/${game._id}`} >
                 <img src={game.imageUrl} alt={game.title} />
             </Link>
+            <div className={styles.buttonControl}>
+                <Link to={`/edit/${game._id}`} state={game} >Edit</Link>
+                <Button variant="primary" onClick={onClickShowDelete} >Delete</Button>
+            </div>
+
+            <DeleteButton
+                {...game}
+                showDelete={showDelete}
+                onClickCloseDelete={onClickCloseDelete}
+                onDeleteGame={onDeleteGame}
+            />
         </div>
     );
 };
