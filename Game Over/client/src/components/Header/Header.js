45e1c0db7f73260bda0f9cbc220ea65788c0b36b
Kalizar <kalinangelov@abv.bg>
2023-07-11 12:11:28 +0300

    Update
    Updating CSS of navigation

--- Game Over/client/src/components/Header/Header.js
+++ Game Over/client/src/components/Header/Header.js
@@ -10,26 +10,24 @@ export const Header = () => {
 
     return (
         <header>
-            <div className={styles.header}>
-                <div className={styles.navigation}>
-                    <h1><Link to='/' >GAME OVER</Link></h1>
+            <div className={styles.navigation}>
+                <h1><Link to='/' >GAME OVER</Link></h1>
 
-                    {isAuthenticated && (
-                        <ul>
-                            <li className={styles.buttonCatalog}> <Link to='/catalog'>Catalog</Link> </li>
-                            <li className={styles.buttonProfile}> <Link to='/profile'>Profile</Link> </li>
-                            <li className={styles.buttonLogout}> <Link to='/logout'>Logout</Link> </li>
-                        </ul>
-                    )}
+                {isAuthenticated && (
+                    <ul>
+                        <li className={styles.navBtn}> <Link to='/catalog'>Catalog</Link> </li>
+                        <li className={styles.navBtn}> <Link to='/profile'>Profile</Link> </li>
+                        <li className={styles.navBtn}> <Link to='/logout'>Logout</Link> </li>
+                    </ul>
+                )}
 
-                    {!isAuthenticated && (
-                        <ul>
-                            <li className={styles.buttonCatalog}> <Link to='/catalog'>Catalog</Link> </li>
-                            <li className={styles.buttonLogin}> <Link to='/login'>Login</Link> </li>
-                            <li className={styles.buttonRegister}> <Link to='/register'>Register</Link> </li>
-                        </ul>
-                    )}
-                </div>
+                {!isAuthenticated && (
+                    <ul>
+                        <li className={styles.navBtn}> <Link to='/catalog'>Catalog</Link> </li>
+                        <li className={styles.navBtn}> <Link to='/login'>Login</Link> </li>
+                        <li className={styles.navBtn}> <Link to='/register'>Register</Link> </li>
+                    </ul>
+                )}
             </div>
         </header>
     );
