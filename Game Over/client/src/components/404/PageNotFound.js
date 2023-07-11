12e339d2a6ea0bc987ef9401a8cf111abb26db4c
Kalizar <kalinangelov@abv.bg>
2023-07-11 12:44:03 +0300

    Update
    Updated CSS of 404 page

--- Game Over/client/src/components/404/PageNotFound.js
+++ Game Over/client/src/components/404/PageNotFound.js
@@ -5,14 +5,13 @@ import { Link } from 'react-router-dom';
 export const PageNotFound = () => {
     return (
         <div className={styles.ups}>
-            
-            <h2>404</h2>
-            <div>
+            <div className={styles.message}>
+                <h2>404</h2>
                 <h3>Page Not Found</h3>
                 <p>
-                    The Page You Are Looking For Doesn't Exist Or Another Error Occurred. Go.......
-                    <Link to='/'>Home</Link>
-                </p>
+                        The Page You Are Looking For Doesn't Exist Or Another Error Occurred. Go.......
+                        <Link to='/'>Home</Link>
+                    </p>
             </div>
         </div>
     );
