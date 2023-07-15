2034e132d708bd74c499bd84adf2a213d6829772
Kalizar <kalinangelov@abv.bg>
2023-07-15 12:57:57 +0300

    Update register page

--- Game Over/client/src/components/Register/Register.js
+++ Game Over/client/src/components/Register/Register.js
@@ -28,7 +28,7 @@ export const Register = () => {
 
             setAuth(response);
             setLoader(false);
-            navigate('/catalog');
+            navigate('/profile');
         } catch (err) {
             errorAlert(err.message);
             setLoader(false);
@@ -43,57 +43,54 @@ export const Register = () => {
                     <p>{errorMessage}</p>
                 </div>
             }
-            
             <div className={styles.brandLogo}></div>
-
             <form className={styles.registerForm} onSubmit={onRegister} >
                 <h1 >Register Form</h1>
-
-                <label htmlFor="username">Username: </label>
-                <input
-                    className={styles.username}
-                    placeholder="Username"
-                    type="text"
-                    name="username"
-                    value={formValue.username}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="email">Email: </label>
-                <input
-                    className={styles.email}
-                    placeholder="Email"
-                    type="text"
-                    name="email"
-                    value={formValue.email}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="password">Password: </label>
-                <input
-                    className={styles.password}
-                    placeholder="Password"
-                    type="password"
-                    name="password"
-                    value={formValue.password}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="rePassword">Confirm Password: </label>
-                <input
-                    className={styles.rePassword}
-                    placeholder="Confirm Password"
-                    type="password"
-                    name="rePassword"
-                    value={formValue.rePassword}
-                    onChange={onFormValueChange}
-                />
-
+                <div className={styles.formSection}> 
+                    <i class="fa-solid fa-user"></i>
+                    <input
+                        placeholder="Username"
+                        type="text"
+                        name="username"
+                        value={formValue.username}
+                        onChange={onFormValueChange}
+                    />
+                </div>               
+                <div className={styles.formSection}> 
+                    <i class="fa-solid fa-at"></i>
+                    <input
+                        placeholder="Email"
+                        type="text"
+                        name="email"
+                        value={formValue.email}
+                        onChange={onFormValueChange}
+                    />
+                </div>               
+                <div className={styles.formSection}> 
+                    <i class="fa-solid fa-key"></i>
+                    <input
+                        placeholder="Password"
+                        type="password"
+                        name="password"
+                        value={formValue.password}
+                        onChange={onFormValueChange}
+                    />
+                </div>     
+                <div className={styles.formSection}> 
+                    <i class="fa-solid fa-key"></i>
+                    <input
+                        placeholder="Confirm Password"
+                        type="password"
+                        name="rePassword"
+                        value={formValue.rePassword}
+                        onChange={onFormValueChange}
+                    />
+                </div>
                 <div>
                     <button className={styles.send}>Register</button>
                 </div>
                 <div className={styles.registerInfo}>
-                    <p> Already Have Account? Click Here! <Link to='/login'>Login</Link> </p>
+                    <p> Already Have Account? Click Here <i class="fa-regular fa-hand-point-right"> </i> <Link to='/login'>Login</Link> </p>
                 </div>
             </form>
         </div>
