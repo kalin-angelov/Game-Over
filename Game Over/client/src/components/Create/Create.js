7777ceadc5fc9fb06307d009f6cb46805808fc10
Kalizar <kalinangelov@abv.bg>
2023-07-15 12:58:59 +0300

    Update create page

--- Game Over/client/src/components/Create/Create.js
+++ Game Over/client/src/components/Create/Create.js
@@ -28,86 +28,94 @@ export const Create = () => {
             <form className={styles.createForm} onSubmit={(e) => onAddNewGameSubmit(e, formValue)}>
                 <h1>Add New Game</h1>
 
-                <label htmlFor="title">Title: </label>
-                <input
-                    className={styles.title}
-                    placeholder="Title"
-                    type="text"
-                    name="title"
-                    value={formValue.title}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="help">Help: </label>
-                <select 
-                    className={styles.help} 
-                    name='help' 
-                    value={formValue.help}
-                    onChange={onFormValueChange}>
-                        <option>Other</option>
-                        <option>Location</option>
-                        <option>Item</option>
-                        <option>Boss</option>
-                        <option>Skills</option>
-                </select>
-
-                <label htmlFor="platform">Platform: </label>
-                <select 
-                    className={styles.platform} 
-                    name='platform' 
-                    value={formValue.platform}
-                    onChange={onFormValueChange}>
-                        <option>Other</option>
-                        <option>PlayStation</option>
-                        <option>Xbox</option>
-                        <option>Nintendo</option>
-                        <option>PC</option>
-                </select>
-
-                <label htmlFor="genre">Genre: </label>
-                <input
-                    className={styles.genre}
-                    placeholder="Genre"
-                    type="text"
-                    name="genre"
-                    value={formValue.genre}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="players">Number Of Players: </label>
-                <input
-                    className={styles.players}
-                    placeholder="Players"
-                    type="text"
-                    name="players"
-                    value={formValue.players}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="imageUrl">ImageUrl: </label>
-                <input
-                    className={styles.imageUrl}
-                    placeholder="ImageUrl"
-                    type="text"
-                    name="imageUrl"
-                    value={formValue.imageUrl}
-                    onChange={onFormValueChange}
-                />
-
-                <label htmlFor="summary">Summary: </label>
-                <textarea
-                    className={styles.summary}
-                    placeholder="Summary"
-                    type="text"
-                    name="summary"
-                    value={formValue.summary}
-                    onChange={onFormValueChange}
-                ></textarea>
-
-                <article>
+                <div className={styles.formSection}>
+                    <i class="fa-solid fa-file-signature"></i>
+                    <input
+                        className={styles.title}
+                        placeholder="Title"
+                        type="text"
+                        name="title"
+                        value={formValue.title}
+                        onChange={onFormValueChange}
+                    />
+                </div>
+                <div className={styles.formSection}>
+                    <i class="fa-brands fa-hire-a-helper"></i>
+                    <select 
+                        className={styles.help} 
+                        name='help' 
+                        value={formValue.help}
+                        onChange={onFormValueChange}>
+                            <option value="" disabled hidden>Help With</option>
+                            <option>Other</option>
+                            <option>Location</option>
+                            <option>Item</option>
+                            <option>Boss</option>
+                            <option>Skills</option>
+                    </select>
+                </div>
+                <div className={styles.formSection}>
+                    <i class="fa-solid fa-gamepad"></i>
+                    <select 
+                        className={styles.platform} 
+                        name='platform' 
+                        value={formValue.platform}
+                        onChange={onFormValueChange}>
+                            <option value="" disabled hidden>Platform</option>
+                            <option>Other</option>
+                            <option>PlayStation</option>
+                            <option>Xbox</option>
+                            <option>Nintendo</option>
+                            <option>PC</option>
+                    </select>
+                </div>
+                <div className={styles.formSection}>
+                    <i class="fa-solid fa-dna"></i>
+                    <input
+                        className={styles.genre}
+                        placeholder="Genre"
+                        type="text"
+                        name="genre"
+                        value={formValue.genre}
+                        onChange={onFormValueChange}
+                    />
+                </div>
+               <div className={styles.formSection}>
+                    <i class="fa-solid fa-people-group"></i>
+                    <input
+                        className={styles.players}
+                        placeholder="Players"
+                        type="text"
+                        name="players"
+                        value={formValue.players}
+                        onChange={onFormValueChange}
+                    />
+               </div>
+               <div className={styles.formSection}>
+                    <i class="fa-regular fa-image"></i>
+                    <input
+                        className={styles.imageUrl}
+                        placeholder="ImageUrl"
+                        type="text"
+                        name="imageUrl"
+                        value={formValue.imageUrl}
+                        onChange={onFormValueChange}
+                    />
+               </div>
+               <div className={styles.formSection}>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                    <textarea
+                        className={styles.summary}
+                        placeholder="Summary"
+                        type="text"
+                        name="summary"
+                        value={formValue.summary}
+                        onChange={onFormValueChange}
+                    ></textarea>
+               </div>
+                <div>
                     <button className={styles.send}>Save</button>
-                </article>
-
+                </div>
             </form>
         </div>
     );
