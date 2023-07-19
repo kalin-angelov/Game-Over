868e61951a48654002a7548195c2bc509e3b6bf2
Kalizar <kalinangelov@abv.bg>
2023-07-19 17:12:59 +0300

    Update Login.js

--- Game Over/client/src/components/Login/Login.js
+++ Game Over/client/src/components/Login/Login.js
@@ -42,7 +42,7 @@ export const Login = () => {
             <form className={styles.loginForm} onSubmit={onLogin} >
                 <h1>Login</h1>
                 <div className={styles.formSection}>
-                    <i class="fa-solid fa-at"></i>
+                    <i className="fa-solid fa-at"></i>
                     <input
                         className={styles.email}
                         placeholder="Email"
@@ -53,7 +53,7 @@ export const Login = () => {
                     />
                 </div>
                 <div className={styles.formSection}>
-                    <i class="fa-solid fa-key"></i>
+                    <i className="fa-solid fa-key"></i>
                     <input
                         className={styles.password}
                         placeholder="Password"
@@ -67,7 +67,7 @@ export const Login = () => {
                     <button className={styles.send}>Login</button>
                 </div>
                 <div className={styles.loginInfo}>
-                    <p> Don't Have An Account? Click Here <i class="fa-regular fa-hand-point-right"> </i> <Link to='/register'>Register</Link> </p>
+                    <p> Don't Have An Account? Click Here <i className="fa-regular fa-hand-point-right"> </i> <Link to='/register'>Register</Link> </p>
                 </div>
             </form >
         </div >
