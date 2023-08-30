e0595d22bb36f991eb5e3a91572477d116d5b82a
Kalizar <kalinangelov@abv.bg>
2023-08-30 18:55:34 +0300

    Update GameInfo.js

--- Game Over/client/src/components/Catalog/GameInfo.js
+++ Game Over/client/src/components/Catalog/GameInfo.js
@@ -8,9 +8,9 @@ export const GameInfo = (game) => {
             <img src={game.imageUrl} alt={game.title} />
 
             <div className={styles.gameInfo}>
-                <h3>Title: {game.title}</h3>
-                <p>Help: {game.help}</p>
-                <p>Platform: {game.platform}</p>
+                <h3>Title: <span>{game.title}</span></h3>
+                <p>Help: <span>{game.help}</span></p>
+                <p>Platform: <span>{game.platform}</span></p>
             </div>
 
             <Link className={styles.detailsBtn} to={`/details/${game._id}`} state={game}>
