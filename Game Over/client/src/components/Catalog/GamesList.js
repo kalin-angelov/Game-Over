ca21cfeac332f1221bf3581d4d469fb4fde9810c
Kalizar <kalinangelov@abv.bg>
2023-08-31 13:37:13 +0300

    Update GamesList.js

--- Game Over/client/src/components/Catalog/GamesList.js
+++ Game Over/client/src/components/Catalog/GamesList.js
@@ -1,7 +1,7 @@
 import styles from "./GamesList.module.css";
 
 import { Link } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useState } from "react";
 
 import { Search } from './Search';
 import { GameInfo } from "./GameInfo";
@@ -9,24 +9,42 @@ import { AuthContext } from '../../contexts/AuthContext';
 
 export const GamesList = () => {
     const { gamesList, isAuthenticated } = useContext(AuthContext);
+    const [ searchResult, setSearchResult ] = useState(null);
+
+    const getSearchResult = (result) => {
+        setSearchResult(result);
+    };
 
     return (
         <>
-           <Search />
-            {(gamesList.length > 0) ?
-                <div className={styles.gameList}>
-                    {gamesList.map(game => <GameInfo key={game._id} {...game} />)}
-                </div>
+           <Search getSearchResult={getSearchResult} />
+            {(searchResult !== null) ?
+
+                (searchResult.length > 0) ?
+                    <div className={styles.gameList}>
+                        {searchResult.map(game => <GameInfo key={game._id} {...game} />)}
+                    </div>
                 :
-                <div className={styles.noGames}>
-                    <h3>Game Catalog Is Empty</h3>
-                    {isAuthenticated && 
-                        <p> Be First To Add New Game! 
-                            <Link to='/register'>Register</Link> 
-                        </p>
-                    }
-                </div>
-            }
+                    <div className={styles.noGames}>
+                        <h3>There Are No Result's</h3>
+                    </div>
+                
+            :
+
+                (gamesList.length > 0) ?
+                    <div className={styles.gameList}>
+                        {gamesList.map(game => <GameInfo key={game._id} {...game} />)}
+                    </div>
+                :
+                    <div className={styles.noGames}>
+                        <h3>Game Catalog Is Empty</h3>
+                        {isAuthenticated && 
+                            <p> Be First To Add New Game! 
+                                <Link to='/register'>Register</Link> 
+                            </p>
+                        }
+                    </div>
+            };
         </>
     );
 };
