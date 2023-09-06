fcee098904c1072b4e2018e5439820c19db43508
Kalizar <kalinangelov@abv.bg>
2023-09-06 15:53:32 +0300

    Update Home.js

--- Game Over/client/src/components/Home/Home.js
+++ Game Over/client/src/components/Home/Home.js
@@ -3,10 +3,11 @@ import styles from './Home.module.css';
 export const Home = () => {
     return (
         <section >   
+            <h1>Welcome To Game Over</h1>
+
             <div className={styles.home}> 
-                <p>Here You Can Find Advice On How To Beat The Game That Is Giving You A Hard Time </p>
-                
                 <img src="images/img.png" alt="homeImage" />
+                <p>Here You Can Find Advice On How To Beat The Game That Is Giving You A Hard Time </p>
             </div>
         </section>
     );
