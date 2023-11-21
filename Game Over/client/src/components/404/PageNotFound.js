f340527a99921f20046b1d74915f68222278b924
Kalizar <kalinangelov@abv.bg>
2023-11-21 16:37:04 +0200

    Update 404

--- Game Over/client/src/components/404/PageNotFound.js
+++ Game Over/client/src/components/404/PageNotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const PageNotFound = () => {
     return (
-        <section className={styles.ups}>
+        <main className={styles.ups}>
             <h2>404</h2>
             <img src="images/404.jpg" alt="Broken controller" />
             <h3>Page Not Found</h3>
@@ -12,6 +12,6 @@ export const PageNotFound = () => {
                 The page you are looking for doesn't exist or another error occurred. Go.......
                 <Link to='/'>Home</Link>
             </p>
-        </section>
+        </main>
     );
 };
