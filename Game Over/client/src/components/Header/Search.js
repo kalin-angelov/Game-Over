ffe5842960fa276f1c9290f7ce71fed90ba167b9
Kalizar <kalinangelov@abv.bg>
2023-11-22 14:08:26 +0200

    Update Search Bar

--- Game Over/client/src/components/Header/Search.js
+++ Game Over/client/src/components/Header/Search.js
@@ -1,41 +1,36 @@
 import styles from './Search.module.css';
 
-import { useForm } from '../../hooks/useForm';
 import { AuthContext } from '../../contexts/AuthContext';
 
-import { useContext } from "react";
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const Search = () => {
     const navigate = useNavigate();
+    const [searchFor, setSearchFor] = useState('');
+    const [searchBy, setSearchBy] = useState('');
     const { gamesList, setSearchResult } = useContext(AuthContext);
-    const { formValue, onFormValueChange } = useForm({
-        searchFor: '',
-        searchBy: ''
-    });
-
-    const onSearch = (e, body) => {
+   
+    const onSearch = (e) => {
         e.preventDefault();
         navigate('/catalog');
         
-        const { searchFor, searchBy } = body;
         let result;
-
         if (searchFor === '') {
             setSearchResult(null);
         } else {
             switch (searchBy) {
-                case "Help":
+                case 'Help':
                     result = gamesList.filter(item => 
                         item.help.toLowerCase()
                         .includes(searchFor.toLowerCase()));
                     break;
-                case "Platform":
+                case 'Platform':
                     result = gamesList.filter(item => 
                         item.platform.toLowerCase()
                         .includes(searchFor.toLowerCase()));
                     break;
-                case "Title":
+                case 'Title':
                     result = gamesList.filter(item => 
                         item.title.toLowerCase()
                         .includes(searchFor.toLowerCase())
@@ -52,27 +47,40 @@ export const Search = () => {
         };
     };
 
+    const onClear = (e) => {
+        e.preventDefault();
+        navigate('/catalog');
+
+        setSearchFor('');
+        setSearchBy('');
+        setSearchResult(null);
+    };
+
     return (
         <section className={styles.search}>
-            <form className={styles.searchForm} onSubmit={(e) => onSearch(e, formValue)}>
-                <i className="fa-solid fa-magnifying-glass"></i>
+            <form className={styles.searchForm} onSubmit={(e) => onSearch(e)}>
+                <label htmlFor='serach'></label>
                 <input 
-                    type="text" 
-                    placeholder="Search"
-                    name="searchFor"
-                    value={formValue.search}
-                    onChange={onFormValueChange}
+                    id='search'
+                    type='text' 
+                    placeholder='Search'
+                    name='searchFor'
+                    role='searchbox'
+                    value={searchFor}
+                    onChange={(e) => {setSearchFor(e.target.value)}}
                 />
+                <button type='button' onClick={(e) => onClear(e)}><i className='fa-solid fa-xmark'></i></button>
                 <select 
-                    name="searchBy"
-                    value={formValue.searchBy}
-                    onChange={onFormValueChange}
+                    name='searchBy'
+                    value={searchBy}
+                    onChange={(e) => {setSearchBy(e.target.value)}}
                 >
-                    <option value="" disabled hidden>Search By</option>
+                    <option value='' disabled hidden>Search By</option>
                     <option>Title</option>
                     <option>Help</option>
                     <option>Platform</option>
                 </select>
+                <button type='submit'><i className='fa-solid fa-magnifying-glass'></i></button>
             </form>
         </section>
     );
