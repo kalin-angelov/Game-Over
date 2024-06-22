b2764314277dbb6b74df89bd181292724629ed56
Kalin Angelov <kalinangelov@abv.bg>
2024-06-22 08:15:09 +0300

    Update GameInfo.js

--- Game-Over/client/src/components/Catalog/GameInfo.js
+++ Game-Over/client/src/components/Catalog/GameInfo.js
@@ -4,25 +4,25 @@ import { Link } from 'react-router-dom';
 
 export const GameInfo = (game) => {
     return (
-        <article className={styles.game}>
+        <div className={styles.game}>
             <img src={game.imageUrl} alt={game.title} loading='lazy'/>
 
             <section className={styles.gameInfo}>
-                <p className={styles.gameTitle} title={game.title}>Title: <span>{game.title}</span></p>
+                <h2 className={styles.gameTitle} title={game.title}>Title: <span>{game.title}</span></h2>
                 <p>Help: <span>{game.help}</span></p>
                 <p>Platform: <span>{game.platform}</span></p>
             </section>
-            <section>
-                <Link className={styles.optionsBtn} to={`/details/${game._id}`}>
+            <ul>
+                <li><Link className={styles.optionsBtn} to={`/details/${game._id}`}>
                     <i className="fa-solid fa-circle-info"></i> 
                     Details
-                </Link>
+                </li></Link>
 
-                <Link className={styles.optionsBtn} to={`/comments/${game._id}`}>
+                <li><Link className={styles.optionsBtn} to={`/comments/${game._id}`}>
                     <i className="fa-solid fa-comment-dots"></i>
                     Comments
-                </Link>
-            </section>
-        </article>
+                </li></Link>
+            </ul>
+        </div>
     );
 };
